41ae794ab647e94108df36981992a5bd13b7ecab
Pedro A. Fabri <pedroafabri@gmail.com>
2024-12-22 13:16:08 -0300

    refactor: 💡 [WIP] Moved tests to module

--- src/password/mod.rs
+++ src/password/mod.rs
@@ -67,6 +67,72 @@ mod tests {
     use super::*;
 
     #[test]
+    fn should_create_new_password() {
+        Password::new();
+    }
+
+    #[test]
+    fn should_get_zeroed_raw_bytes() {
+        let pass = Password::new();
+        let bytes = pass.get_raw_bytes();
+        let expected = [0u8; 18];
+        assert!(bytes.iter().eq(expected.iter()));
+    }
+
+    #[test]
+    fn should_return_error_when_setting_byte_out_of_bounds() {
+        let mut pass = Password::new();
+        assert!(pass.set_byte_value(18, 0x0F).is_err());
+    }
+
+    #[test]
+    fn should_set_whole_byte() {
+        let mut pass = Password::new();
+        assert!(!pass.set_byte_value(2, 0x0F).is_err());
+        assert_eq!(pass.bytes[2], 0x0F);
+    }
+
+    #[test]
+    fn should_return_error_when_setting_bit_out_of_bounds() {
+        let mut pass = Password::new();
+        let r = pass.set_bit(128);
+        assert!(matches!(r, Err(BitOutOfBoundsError)));
+    }
+
+    #[test]
+    fn should_set_last_bit_of_byte() {
+        let mut pass = Password::new();
+        let r = pass.set_bit(127);
+        assert!(!r.is_err());
+        assert_eq!(pass.bytes[15], 1);
+    }
+
+    #[test]
+    fn should_set_first_and_last_bits_of_byte() {
+        let mut pass = Password::new();
+        assert!(!pass.set_bit(127).is_err());
+        assert!(!pass.set_bit(120).is_err());
+        assert_eq!(pass.bytes[15], 0b10000001);
+    }
+
+    #[test]
+    fn should_clear_last_bit_of_byte() {
+        let mut pass = Password::new();
+        pass.bytes[15] = 0xFF;
+        assert!(!pass.clear_bit(127).is_err());
+        assert_eq!(pass.bytes[15], 0b11111110);
+    }
+
+    #[test]
+    fn should_clear_first_and_last_bits_of_byte() {
+        let mut pass = Password::new();
+        pass.bytes[15] = 0xFF;
+        assert!(!pass.clear_bit(120).is_err());
+        assert!(!pass.clear_bit(127).is_err());
+        assert_eq!(pass.bytes[15], 0b01111110);
+    }
+
+    #[test]
     fn should_get_byte_from_bit() {
         let password = Password::new();
 
